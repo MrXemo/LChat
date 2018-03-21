a30eda231708b6eb02f02a9d9a40068a8e4ebcc2
zhong <liuzhiping@hboxs.com>
2018-03-21 17:29:59 +0800

    add

---
+++ src/main/java/com/zome/lchat/service/UserService.java
@@ -0,0 +1,9 @@
+package com.zome.lchat.service;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class UserService {
+}
